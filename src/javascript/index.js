9b4c5d2d69fbdddd65eab5a7402e2917f716535a
Charley Gooch <charleygooch@Charleys-MacBook-Pro.local>
2018-11-06 12:45:23 +0000

    updated js file and html to attempt data post

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,11 +1,12 @@
-var temp = null;
+var temp = 0;
 var powerSaver = false;
 var thermo = null;
 
-function Thermo(){}
+function Thermo(){
+}
 
 function getTemp(){
-    return temp;
+    return thermo.temp;
 }
 
 function createThermo(){
@@ -13,52 +14,52 @@ function createThermo(){
     thermo.init();
 }
 Thermo.prototype.init = function() {
-    temp = 20;
+    thermo.temp = 20;
 }
 
 Thermo.prototype.increaseTemp = function() {
     if(powerSaver) {
-        if(temp == 25) {
+        if(thermo.temp == 25) {
             alert("With power saver enabled, maximum temperature is 25 degrees. To increase, please disable power saver");
         }else{
-            temp += 1;
+            thermo.temp += 1;
         }
     }else{
-        if(temp == 32) {
+        if(thermo.temp == 32) {
             alert("Maximum temperature is 32 degrees.");
         }else{ 
-            temp += 1;
+            thermo.temp += 1;
         }
     }
 }
 
 Thermo.prototype.decreaseTemp = function() {
-    if(temp == 10){
+    if(thermo.temp == 10){
         alert("Minimum temperature is 10 degrees.");
     }else{
-        temp -= 1;
+        thermo.temp -= 1;
     }
 }
 
 Thermo.prototype.togglePowerSaver = function() {
     if(powerSaver = false){
         powerSaver == true;
-        if(temp > 25){
-            temp = 25;
+        if(thermo.temp > 25){
+            thermo.temp = 25;
         }
     }else{
         powerSaver == false;
     }
 }
 
-Thernmo.prototype.resetTemp = function() {
-    temp = 20;
+Thermo.prototype.resetTemp = function() {
+    thermo.temp = 20;
 }
 
 Thermo.prototype.getEnergyUsage = function(){
-    if(temp < 18) {
+    if(thermo.temp < 18) {
         return 'L'
-    } else if(temp < 25) {
+    } else if(thermo.temp < 25) {
         return 'M'
     } else {
         return 'H'
