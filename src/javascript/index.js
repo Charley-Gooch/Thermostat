5de49cef45cd10eba15ca6468e5d16faad89757e
Charley Gooch <charleygooch@Charleys-MacBook-Pro.local>
2018-11-07 09:57:26 +0000

    added power saver feature + toggle

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,5 +1,5 @@
 var temp = 0;
-var powerSaver = true;
+var powerSaver = false;
 var thermo = null;
 
 function Thermo(){
@@ -14,24 +14,29 @@ function getTemp(){
     return thermo.temp;
 }
 
+function getPowerSaver(){
+    return thermo.powerSaver;
+}
+
 Thermo.prototype.init = function() {
     thermo.temp = 20;
+    thermo.powerSaver = true;
 }
 
 Thermo.prototype.increaseTemp = function() {
-    if(powerSaver) {
+    if(thermo.powerSaver) {
         if(thermo.temp == 25) {
             alert("With power saver enabled, maximum temperature is 25 degrees. To increase, please disable power saver");
         }else{
             thermo.temp += 1;
-            document.getElementById('thermo').innerHTML = "Temperature: " + getTemp() + " ";
+            document.getElementById('thermo').innerHTML = "Temperature: " + getTemp() + " Degrees";
         }
     }else{
         if(thermo.temp == 32) {
             alert("Maximum temperature is 32 degrees.");
         }else{ 
             thermo.temp += 1;
-            document.getElementById('thermo').innerHTML = "Temperature: " + getTemp() + " ";
+            document.getElementById('thermo').innerHTML = "Temperature: " + getTemp() + " Degrees";
             
         }
     }
@@ -42,25 +47,26 @@ Thermo.prototype.decreaseTemp = function() {
         alert("Minimum temperature is 10 degrees.");
     }else{
         thermo.temp -= 1;
-        document.getElementById('thermo').innerHTML = "Temperature: " + getTemp() + " ";
+        document.getElementById('thermo').innerHTML = "Temperature: " + getTemp() + " Degrees";
     }
 }
 
 Thermo.prototype.togglePowerSaver = function() {
-    if(powerSaver = false){
-        powerSaver == true;
+    if(thermo.powerSaver == false){
+        thermo.powerSaver = true;
         if(thermo.temp > 25){
             thermo.temp = 25;
-            document.getElementById('thermo').innerHTML = "Temperature: " + getTemp() + " ";
+            document.getElementById('thermo').innerHTML = "Temperature: " + getTemp() + " Degrees";
         }
-    }else{
-        powerSaver == false;
+    }else if(thermo.powerSaver == true){
+        thermo.powerSaver = false;
     }
+    document.getElementById('powSav').innerHTML = "Power Saver Status: " + getPowerSaver();
 }
 
 Thermo.prototype.resetTemp = function() {
     thermo.temp = 20;
-    document.getElementById('thermo').innerHTML = "Temperature: " + getTemp() + " ";
+    document.getElementById('thermo').innerHTML = "Temperature: " + getTemp() + " Degrees";
 }
 
 Thermo.prototype.getEnergyUsage = function(){
